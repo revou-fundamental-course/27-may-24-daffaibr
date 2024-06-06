64e0e60bddd6f542654e913ed830224fccf15fe4
Daffa <daflot2005@gmail.com>
2024-06-06 20:37:59 +0700

    Another Commit

--- js/script.js
+++ js/script.js
@@ -1,33 +1,25 @@
 // Ini JavaScript
 
-function formValidation() {
-    let nameinput = document.getElementById('name').value;
-    console.log(nameInput);
-    if (nameInput == null || nameInput == '') {
-        alert("Inputan Kosong");
-    } else {
-        console.log(nameInput);
-    }
+function randomString(len,charSet)	{
+	charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	var randomString = '';
+	for (var i = 0; i < len; i++){
+		var randomPoz = Math.floor(Math.random() * charSet.length);
+		randomString += charSet.substring(randomPoz,randomPoz+1);
+	}
+	return randomString;
 }
 
-let indexSlide = 1;
-showBanner(1);
+function cekCaptcha(){
+	var inputUser = document.querySelector(".input");
+	var captcha = document.querySelector(".box").innerHTML;
 
-function nextSlide(n) {
-    showBanner(indexSlide += n);
+	if(inputUser.value === captcha){ // jika captcha benar jalankan fungsi di bawah ini
+		alert('captcha benar');
+
+	}else if (inputUser.value.length === 5 && inputUser.value != captcha){ // jika captcha salah kosongkan bidang input dan acak captcha
+		inputUser.value ='';
+		document.querySelector(".box").innerHTML = randomString(5);
+
+	}
 }
-
-function showBanner(indexBanner) {
-   let listImage = document.getElementsByClassName('bg-image');
-   if (index > listImage.length) indexSlide = 1;
-
-   let index = 0;
-   while (index < listImage.length) {
-        listImage[index].style.display = 'none';
-    index++;
-   }
-
-   listImage[indexSlide - 1].style.display = 'block';
-}
-
-setInterval(() => nextSlide(1), 3000);
